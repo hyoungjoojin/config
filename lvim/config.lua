9b8505c2eaf1699f1385cec4e9f2ffb50d94ea30
Hyoungjoo Jin <hyoungjoo.j@gmail.com>
2025-02-08 22:34:28 +0900

    add lunarvim config.lua

---
+++ lvim/config.lua
@@ -0,0 +1,37 @@
+-- Banner
+lvim.builtin.alpha.dashboard.section.header.val = {}
+
+-- Preferences
+vim.opt.relativenumber = true
+
+lvim.colorscheme = "gruvbox-material"
+
+lvim.plugins = {
+  { 'sainnhe/gruvbox-material' },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+    },
+  }
+}
+
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {}
+lvim.format_on_save = true
+
+lvim.keys.normal_mode["<C-h>"] = "<cmd>TmuxNavigateLeft<CR>"
+lvim.keys.normal_mode["<C-j>"] = "<cmd>TmuxNavigateDown<CR>"
+lvim.keys.normal_mode["<C-k>"] = "<cmd>TmuxNavigateUp<CR>"
+lvim.keys.normal_mode["<C-l>"] = "<cmd>TmuxNavigateRight<CR>"
+
+lvim.builtin.which_key.mappings["-"] = {
+  "<cmd>vsplit<CR>", "Vertical Split"
+}
+
+lvim.builtin.which_key.mappings["_"] = {
+  "<cmd>split<CR>", "Horizontal Split"
+}
